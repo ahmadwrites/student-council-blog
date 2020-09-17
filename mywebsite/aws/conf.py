2fa52b7f9b632fe346ef7177c8ef81985d0618bf
ahmad <ahmadhasnol@hotmail.com>
2020-09-17 23:29:56 +0800

    Adsense

--- mywebsite/aws/conf.py
+++ mywebsite/aws/conf.py
@@ -1,13 +1,13 @@
-# AWS_ACCESS_KEY_ID = 'AKIAZJN73NTXNBWV4XUU'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'xmum-cs'
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-#
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-#
-# AWS_LOCATION = 'static'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# DEFAULT_FILE_STORAGE = 'mywebsite.aws.storage_backends.MediaStorage'
+AWS_ACCESS_KEY_ID = 'AKIAZJN73NTXNBWV4XUU'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'xmum-cs'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+DEFAULT_FILE_STORAGE = 'mywebsite.aws.storage_backends.MediaStorage'
